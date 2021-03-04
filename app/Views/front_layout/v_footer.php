fb7710b3aa3aa319110288cbbc9129cac8c5cd32
Wempy Aditya <adityawempy@gmail.com>
2021-03-04 15:15:23 +0700

    fix footer section

--- app/Views/front_layout/v_footer.php
+++ app/Views/front_layout/v_footer.php
@@ -1,16 +1,37 @@
-        <a id="backToTop" href="#" onclick="backToTop(event);" class="btn btn-success bg-hijau btn back-to-top">
+        <a id="backToTop" href="#" onclick="backToTop(event);" class="btn btn-light bg-hijau btn back-to-top">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
             </svg>
         </a>
             <footer class="footer mt-auto py-3 mt-5"  style="background-color: #5c8d89;">
-                <div class="container">
+                <div class="container text-light">
+                    <div class="row">
+                        <div class="col-sm-5">
+                            Beristighfar merupakan cerminan akan kesadaran diri seseorang yang bertakwa tentang betapa banyak kesalahannya, kemudian memohon ampun kepada Allah agar kembali kepada kebenaran.
+                        </div>
+                        <div class="col-sm-4">
+                            <ul style="list-style: none;">
+                                <li>home</li>
+                                <li>home</li>
+                                <li>home</li>
+                                <li>home</li>
+                            </ul>
+                        </div>
+                        <div class="col-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-bookmarks-fill" viewBox="0 0 20 20">
+                                <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z"/>
+                                <path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z"/>
+                            </svg><br>
+                            Ramadhan Website
+                        </div>
+                    </div>
+                    <hr style="color:white;height:2px;">
                     <div class="row">
                         <div class="col">
-                            <span class="text-light">&copy; Copyright <?php echo(date('Y')); ?> <a href="https://wempy.tech" target="_blank" rel="noopener noreferrer" class="text-light">Wempy.Tech</a></span>
+                            <span class="">&copy; Copyright <?php echo(date('Y')); ?> by <a href="https://wempy.tech" target="_blank" rel="noopener noreferrer" class="text-light">Wempy.Tech</a></span>
                         </div>
                         <div class="col">
-                            <span class="text-light d-flex justify-content-end">V 2.0.0</span>
+                            <span class="d-flex justify-content-end">V 2.0.0</span>
                         </div>
                     </div>
                 </div>
