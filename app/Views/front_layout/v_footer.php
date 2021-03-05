db8b864b9e2802ba9a1ad356304f89daeca7ab10
Wempy Aditya <adityawempy@gmail.com>
2021-03-05 11:12:16 +0700

    change footer style

--- app/Views/front_layout/v_footer.php
+++ app/Views/front_layout/v_footer.php
@@ -6,32 +6,51 @@
             <footer class="footer mt-auto py-3 mt-5"  style="background-color: #5c8d89;">
                 <div class="container text-light">
                     <div class="row">
-                        <div class="col-sm-5">
-                            Beristighfar merupakan cerminan akan kesadaran diri seseorang yang bertakwa tentang betapa banyak kesalahannya, kemudian memohon ampun kepada Allah agar kembali kepada kebenaran.
-                        </div>
-                        <div class="col-sm-4">
-                            <ul style="list-style: none;">
-                                <li>home</li>
-                                <li>home</li>
-                                <li>home</li>
-                                <li>home</li>
-                            </ul>
-                        </div>
-                        <div class="col-3">
+                        <div class="col-lg-3 col-md-12 pt-3 pb-2 text-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-bookmarks-fill" viewBox="0 0 20 20">
                                 <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z"/>
                                 <path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z"/>
                             </svg><br>
-                            Ramadhan Website
+                            <h6><b>Ramadhan</b> Website</h6>
+                        </div>
+                        <div class="col-lg-5 col-md-12 pt-3 pb-2 ps-3 pe-3">
+                            <h5><b>Tentang Website</b></h5>
+                            <p>
+                                Ini adalah Website yang dibuat oleh Wempy.Tech spesial untuk bulan Ramadhan. 
+                                Website ini memiliki fitur hitung mundur bulan Ramadhan dan Idul Fitri, 
+                                Jadwal Shalat untuk seluruh daerah di Indonesia, Al-Quran Online, Murottal, 
+                                dan beberapa fitur lainnya.
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-12 pt-3 pb-2 ps-3 pe-3">
+                            <div class="row">
+                                <h5><b>Peta Situs</b></h5>
+                                <div class="col">
+                                    <ul class="list-unstyled">
+                                        <li><a href="" class="text-light footerLink">Home</a></li>
+                                        <li><a href="" class="text-light footerLink">Al-Quran</a></li>
+                                        <li><a href="" class="text-light footerLink">Murottal</a></li>
+                                        <li><a href="" class="text-light footerLink">Asmaul-Husna</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col">
+                                    <ul class="list-unstyled">
+                                        <li><a href="" class="text-light footerLink">Home</a></li>
+                                        <li><a href="" class="text-light footerLink">Al-Quran</a></li>
+                                        <li><a href="" class="text-light footerLink">Murottal</a></li>
+                                        <li><a href="" class="text-light footerLink">Asmaul-Husna</a></li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <hr style="color:white;height:2px;">
+                    <hr style="color:white;height:4px;">
                     <div class="row">
                         <div class="col">
                             <span class="">&copy; Copyright <?php echo(date('Y')); ?> by <a href="https://wempy.tech" target="_blank" rel="noopener noreferrer" class="text-light">Wempy.Tech</a></span>
                         </div>
-                        <div class="col">
-                            <span class="d-flex justify-content-end">V 2.0.0</span>
+                        <div class="col text-end">
+                            <span class="">V 2.1.0</span>
                         </div>
                     </div>
                 </div>
